2a2e91eb27b70e683dc23fda44585b9d678dd926
Aleksey P <aleksey.p10n@gmail.com>
2025-11-22 09:27:50 +0300

    refactor

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,14 +3,13 @@ import { Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma.service';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CacheModule.register()],
+  imports: [CacheModule.register(), PrismaModule],
   controllers: [AppController],
   providers: [
     AppService,
-    PrismaService,
     {
       provide: APP_INTERCEPTOR,
       useClass: CacheInterceptor,
